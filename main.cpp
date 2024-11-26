d97955a91134926a9ad252e694922c5f3aac4fdf
Demonmasterlqx <183842220@qq.com>
2024-11-26 22:15:09 +0800

    开始施工,part one,检测侧面

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <opencv2/opencv.hpp>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <opencv2/imgproc.hpp>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void processe_image(){
+    cv::Mat image=cv::imread()
+}
+
+int main(){
+    
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+## 工程视觉企划
+
+### 目标检测
+
+#### 检测矿石
+
+##### 检测正上方
+
+![TOP_interface](Picture/FOR_READ_ME/TOP_interface.png)
+
+##### 检测侧边
+
+![SIDE_interface](Picture/FOR_READ_ME/SIDE_interface.png)
+
+##### 检测地面(识别面)
+
+![UNDERSIED](Picture/FOR_READ_ME/UNDERSIED.png)
